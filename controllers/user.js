60e8a2ff282e988ff67a9b4b0d5da4ec6943b797
arvindh07 <arvindhganesh11@gmail.com>
2024-02-25 10:50:41 +0530

    add login controller

--- controllers/user.js
+++ controllers/user.js
@@ -1,10 +1,23 @@
+const bcrypt = require("bcryptjs");
+const User = require("../models/user");
+
 const showLoginPage = (req, res) => {
     return res.render("Login.ejs");
 }
-const handleLogin = (req, res) => {
-    return res.json({
-        msg: "Login"
+const handleLogin = async (req, res) => {
+    const {name, email, password} = req.body;
+    if(!name || !email || !password){
+        return res.status(400).json({
+            error: "All fields required"
+        })
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+        name,
+        email,
+        password: hashedPassword
     })
+    return res.redirect("/");
 }
 
 const handleSignup = (req, res) => {
--- models/user.js
+++ models/user.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
     password: {
         type: String,
         required: true
+    },
+    role: {
+        type: String,
+        enum: ["ADMIN", "USER"],
+        default: "USER"
     }
 }, { timestamps: true })
 

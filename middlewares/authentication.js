a017cf6e4e38cf09f5f8bc3f98ead38935b01bfa
arvindh07 <arvindhganesh11@gmail.com>
2024-02-27 19:23:23 +0530

    add check for auth

--- middlewares/authentication.js
+++ middlewares/authentication.js
@@ -1,12 +1,13 @@
+// for each and every request that is incoming, below function will run
 const checkForAuth = () => {
     return (req, res, next) => {
-        console.log("auth running...",req.url, req.cookies);
-        // req.user = null;
-        // if(!req.cookies["auth_token"]){
-        //     return next();
-        // }
-        // const user = req.cookies["auth_token"];
-        // req.user = user;
+        console.log("auth running...",req.url);
+        req.user = null;
+        if(!req.cookies["auth_token"]){
+            return next();
+        }
+        const user = req.cookies["auth_token"];
+        req.user = user;
         return next();
     }
 }

48625108deb9b0a5a39d908f615e826c60a0b56a
arvindh07 <arvindhganesh11@gmail.com>
2024-03-08 17:19:34 +0530

    created blog model

---
+++ models/blog.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const blogSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    content: {
+        type: String,
+        required: true
+    },
+    coverPic: {
+        type: String,
+        required: true
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    }
+}, { timestamps: true })
+
+module.exports = mongoose.model("Blog", blogSchema);
